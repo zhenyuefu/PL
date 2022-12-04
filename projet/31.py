92fcf1a11400da431364bf74a0bf8b9dadf8929e
lincharles123 <a799802224@gmail.com>
2022-12-04 16:47:10 +0100

    Update 31.py

--- projet/31.py
+++ projet/31.py
@@ -2,8 +2,6 @@ import gurobipy as gp
 import numpy as np
 from gurobipy import GRB
 
-from utils import print_solution
-
 def selection_multicritere_projet(c, budget, w, u):
     """
     :param c: liste des coûts des projets
@@ -35,7 +33,7 @@ def selection_multicritere_projet(c, budget, w, u):
                         GRB.MAXIMIZE)
         MODEL.optimize()
         
-        return MODEL
+        return MODEL, x, z
 
     except gp.GurobiError as e:
         print('Error code ' + str(e.errno) + ': ' + str(e))
@@ -57,19 +55,18 @@ def selection_multicritere_projet_moyenne(c, budget, u):
 
         # ajout des variables
         x = MODEL.addVars(p, vtype=GRB.BINARY, name="x") # projet auquel on attribue le budget
-        z = MODEL.addVars(n, vtype=GRB.CONTINUOUS, name="z")
+        z = [sum(u[i][j] * x[j] for j in range(p)) for i in range(n)]
 
         # ajout des contraintes
         MODEL.update()
         MODEL.addConstrs((x[i] <= 1 for i in range(p))) # contrainte x <= 1
         MODEL.addConstr((gp.quicksum(c[i] * x[i] for i in range(p)) <= budget)) # contrainte de budget
-        MODEL.addConstrs((z[i] == gp.quicksum(u[i][j] * x[j] for j in range(p)) for i in range(n))) # contrainte sur les utilités
 
         # ajout de la fonction objectif
         MODEL.setObjective(gp.quicksum(z[i] for i in range(n)) / n, GRB.MAXIMIZE)
         MODEL.optimize()
 
-        return MODEL
+        return MODEL, x, z
 
     except gp.GurobiError as e:
         print('Error code ' + str(e.errno) + ': ' + str(e))
@@ -77,6 +74,20 @@ def selection_multicritere_projet_moyenne(c, budget, u):
     except AttributeError:
         print('Encountered an attribute error')
 
+def print_solution(MODEL, x, z):
+    if MODEL.status == GRB.OPTIMAL:
+            print("solution:")
+            for i in range(len(z)):
+                print(f"z[{i+1}] = {z[i].getValue()}")
+            print()
+            for i in range(len(x)):
+                print(f"x[{i+1}] = {x[i].x}", end=" ")
+                print()
+            print("objective value = ", MODEL.objVal)
+    else:
+        print("No solution")
+    # print solution time
+    print("solution time = ", MODEL.Runtime, "s")
 
 u = np.array([[19, 6, 17, 2],
               [2, 11, 4, 18]])
@@ -85,17 +96,17 @@ budget = 100
 w1 = [2, 1]
 w2 = [10, 1]
 
-selection_1 = selection_multicritere_projet(c, budget, w1, u)
-selection_2 = selection_multicritere_projet(c, budget, w2, u)
-selection_moyenne = selection_multicritere_projet_moyenne(c, budget, u)
+selection_1,z1,x1 = selection_multicritere_projet(c, budget, w1, u)
+selection_2,z2,x2 = selection_multicritere_projet(c, budget, w2, u)
+selection_moyenne,z3,x3 = selection_multicritere_projet_moyenne(c, budget, u)
 
 print("w =", w1)
 print("Solution avec w1:")
-print_solution(selection_1)
+print_solution(selection_1, z1, x1)
 
 print("\nw =", w2)
 print("Solution avec w2 =:")
-print_solution(selection_2)
+print_solution(selection_2, z2, x2)
 
 print("\nSolution maximisant la satisfaction moyenne des objectifs:")
-print_solution(selection_moyenne)
+print_solution(selection_moyenne, z3, x3)
